80e143b924685dc1ce2d14f75a69d23c69f60f04
khanq1 <quangkha91@gmail.com>
2024-05-18 11:30:52 +0700

    update s3 front-end

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www arn:aws:s3:::myawsbucket-844723630086
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html arn:aws:s3:::myawsbucket-844723630086
+aws s3 cp --recursive --acl public-read ./www myawsbucket-844723630086
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html myawsbucket-844723630086
